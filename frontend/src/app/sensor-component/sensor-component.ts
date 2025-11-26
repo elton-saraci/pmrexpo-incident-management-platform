f9be371beaf0c1a3338e277c3cfc1693585a339b
TobiasHillesheim <tobiashillesheim@gmx.de>
2025-11-26 12:28:53 +0100

    add sensorComponent

---
+++ frontend/src/app/sensor-component/sensor-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sensor-component',
+  imports: [],
+  templateUrl: './sensor-component.html',
+  styleUrl: './sensor-component.scss',
+})
+export class SensorComponent {
+
+}
